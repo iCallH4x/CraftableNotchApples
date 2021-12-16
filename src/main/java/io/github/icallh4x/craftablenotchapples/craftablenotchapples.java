34853bd5c3877a1fff565ce030f561d82773fc52
iCallH4x <64992884+iCallH4x@users.noreply.github.com>
2021-12-16 18:09:04 +0000

    Initial commit

---
+++ src/main/java/io/github/icallh4x/craftablenotchapples/craftablenotchapples.java
@@ -0,0 +1,31 @@
+package io.github.icallh4x.craftablenotchapples;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class craftablenotchapples extends JavaPlugin {
+
+
+    public void onEnable() {
+
+        ItemStack apple = new ItemStack(Material.ENCHANTED_GOLDEN_APPLE, 1);
+
+        ShapedRecipe notchApple = new ShapedRecipe(new NamespacedKey(this, "CustomItem"), apple);
+
+        notchApple.shape("***", "*A*", "***");
+
+        notchApple.setIngredient('*', Material.GOLD_BLOCK);
+        notchApple.setIngredient('A', Material.APPLE);
+
+        getServer().addRecipe(notchApple);
+
+    }
+
+    public void onDisable() {
+
+    }
+}
